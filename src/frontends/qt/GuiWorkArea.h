259c89f45edd4a13fe63fa759fa80b06b2ee4e9a
Daniel Ramoeller <d.lyx@web.de>
2021-02-27 07:05:54 +0100

    Extend tab context menu features
    Add
    
    - Close Other Tabs
    - Close Tabs to Left/Right
    - Move Tab to Start/End
    - Show Enclosing Folder
    
    to the tabs context menus.
    
    Fix for bug #11963
    
    (cherry picked from commit a114f12868f8b48b9507aa22bf4156af0062ac97)

--- src/frontends/qt/GuiWorkArea.h
+++ src/frontends/qt/GuiWorkArea.h
@@ -242,6 +242,18 @@ public Q_SLOTS:
 	void closeCurrentBuffer();
 	/// hide current tab, or the one given by \c clicked_tab_
 	void hideCurrentTab();
+	///
+	bool closeTabsToRight();
+	///
+	bool closeTabsToLeft();
+	///
+	void closeOtherTabs();
+	///
+	void moveToStartCurrentTab();
+	///
+	void moveToEndCurrentTab();
+	///
+	bool openEnclosingFolder();
 	/// close the tab given by \c index
 	void closeTab(int index);
 	///
@@ -268,6 +280,9 @@ private:
 
 	/// true if position is a tab (rather than the blank space in tab bar)
 	bool posIsTab(QPoint position);
+
+	// true if there are tabs to the right of the tab at index
+	bool hasTabsToRight(int index);
 
 	int clicked_tab_;
 	///
