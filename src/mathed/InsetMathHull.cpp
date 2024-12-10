kojiOnHill/lyx

commit 5e8578837fea0321998eac04222102986261666d

Avoid some full metrics computations related to math previews
When entering/leaving a math hull inset, a Update::Force flag was set,
in case the metrics of the inset would change because of a switch
between normal and preview representation.

When entering the inset, this code is now used only when the inset was
in preview mode.

In both cases, Update::Force is replaced with Update::SinglePar.

This requites in Text::dispatch to honor Update::SinglePar when it has
been set by some lfun, even when singleparupdate is false.

Part of bug #12297.

src/mathed/InsetMathHull.cpp
@@ -902,7 +902,7 @@ bool InsetMathHull::notifyCursorLeaves(Cursor const & old, Cursor & cur)
 {
 	if (RenderPreview::previewMath()) {
 		reloadPreview(old);
-		cur.screenUpdateFlags(Update::Force);
+		cur.screenUpdateFlags(Update::SinglePar);
 	}
 	return false;
 }
@@ -2273,14 +2273,17 @@ void InsetMathHull::handleFont2(Cursor & cur, docstring const & arg)
 
 void InsetMathHull::edit(Cursor & cur, bool front, EntryDirection entry_from)
 {
+	bool const has_preview = previewState(&cur.bv());
 	cur.push(*this);
 	bool enter_front = (entry_from == Inset::ENTRY_DIRECTION_LEFT ||
 		(entry_from == Inset::ENTRY_DIRECTION_IGNORE && front));
 	enter_front ? idxFirst(cur) : idxLast(cur);
-	// The inset formula dimension is not necessarily the same as the
-	// one of the instant preview image, so we have to indicate to the
-	// BufferView that a metrics update is needed.
-	cur.screenUpdateFlags(Update::Force);
+	if (has_preview) {
+		// The inset formula dimension is in general different from the
+		// one of the instant preview image, so we have to indicate to the
+		// BufferView that a metrics update is needed.
+		cur.screenUpdateFlags(Update::SinglePar);
+	}
 }
 
 

src/Text.cpp
@@ -6386,7 +6386,7 @@ void Text::dispatch(Cursor & cur, FuncRequest & cmd)
 
 	// FIXME: the following code should go in favor of fine grained
 	// update flag treatment.
-	if (singleParUpdate) {
+	if (singleParUpdate || cur.result().screenUpdate() & Update::SinglePar) {
 		// Inserting characters does not change par height in general. So, try
 		// to update _only_ this paragraph. BufferView will detect if a full
 		// metrics update is needed anyway.

commit e3ec44ffe78c1a22250c9ec22be8645077126b1d

Fixup 5e857883: update screen when clicking away from preview
This is a regression in 5e857883 that comes from the fact that the
Update::singlePar flag does not operate on the paragraph that may be
modified but on the one where the cursor currently lies.

Until this is fixed, we do a full update when this situation is encountered.

Fixes bug #13128.

src/mathed/InsetMathHull.cpp
@@ -908,7 +908,15 @@ bool InsetMathHull::notifyCursorLeaves(Cursor const & old, Cursor & cur)
 {
 	if (RenderPreview::previewMath()) {
 		reloadPreview(old);
-		cur.screenUpdateFlags(Update::SinglePar);
+		/** FIXME: currently, SinglePar operates on the current
+		 * paragraph at processUpdateFlags time (here cur) and not the
+		 * paragraph where the change happened (old). When this is
+		 * fixed, the following test will become useless.
+		 */
+		if (&old.innerParagraph() == &cur.innerParagraph())
+			cur.screenUpdateFlags(Update::SinglePar);
+		else
+			cur.screenUpdateFlags(Update::Force);
 	}
 	return false;
 }