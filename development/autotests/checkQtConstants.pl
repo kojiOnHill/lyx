f52177fff8b6a60e851ac1d4803519254cb12f21
Kornel Benko <kornel@lyx.org>
2025-04-01 10:42:52 +0200

    Restore checking of QT constants
    This essentially reverts commit e7dd6b67

--- development/autotests/checkQtConstants.pl
+++ development/autotests/checkQtConstants.pl
@@ -33,7 +33,7 @@ sub checkConstants($) {
     my $lineno = 0;
     while (my $l = <$FI>) {
       $lineno += 1;
-      if ($l =~ /\bQ([a-zA-Z]+\:\:){3}/) {
+      if ($l =~ /\bQ([a-zA-Z]+\:\:){2}/) {
         print "$ui:$lineno $l";
         $no_founds += 1;
       }
