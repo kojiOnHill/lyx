dc02e307979ad34684dbea1cdc56de1ca870cd03
Juergen Spitzmueller <spitz@lyx.org>
2025-04-16 07:57:05 +0200

    Fix endless recursion and string detection mismatch in lyx2lyx (#13168)

--- lib/lyx2lyx/lyx2lyx_tools.py
+++ lib/lyx2lyx/lyx2lyx_tools.py
@@ -116,14 +116,17 @@ def add_to_preamble(document, text):
       if i == -1:
         break
       # we need a perfect match
+      # ignoring surrounding whitespace, though (see #13168)
       matched = True
       for line in text:
-        if i >= prelen or line != document.preamble[i]:
+        if i >= prelen or line.strip() != document.preamble[i].strip():
           matched = False
           break
         i += 1
       if matched:
         return
+      else:
+        i += 1
 
     document.preamble.extend(["% Added by lyx2lyx"])
     document.preamble.extend(text)
