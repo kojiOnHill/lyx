8550e5b3cff4edda62016aa9fcc1b197798316a7
Richard Kimberly Heck <rikiheck@lyx.org>
2025-05-16 13:54:33 -0400

    Fix bug #13187
    No status entry needed, since this was never released.
    
    (cherry picked from commit d8da6760bcd4329a87717492446a868456173991)

--- src/frontends/qt/GuiApplication.cpp
+++ src/frontends/qt/GuiApplication.cpp
@@ -2636,8 +2636,9 @@ void GuiApplication::createView(QString const & geometry_arg, bool autoShow,
 		d->global_menubar_->releaseKeyboard();
 
 	// need to reset system input method coords with the preserved one
-	// when the new view is the second one or later
-	if (d->views_.size() > 0)
+	// when the new view is the second one or later and a document is
+	// open in the current view
+	if (d->views_.size() > 0 && current_view_->currentWorkArea())
 		current_view_->currentWorkArea()->resetInputItemGeometry(true);
 
 	// create new view
