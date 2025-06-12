231b45a06e6665aaac429509a7cfbd4d9790c9d4
Juergen Spitzmueller <spitz@lyx.org>
2025-06-12 11:23:37 +0200

    Amend 59b8c209c4
    The escaped percent char irritates clang, and we only need
    a suitably wide character here.

--- src/frontends/qt/GuiView.cpp
+++ src/frontends/qt/GuiView.cpp
@@ -730,12 +730,9 @@ GuiView::GuiView(int id)
 	zoom_value_->setForegroundRole(statusBar()->foregroundRole());
 	zoom_value_->setFixedHeight(fm.height());
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 11, 0))
-	// The escape is intended (see commit 59b8c209 and #12301),
-	// although it leads to the following Clang warning:
-	//   warning: use of non-standard escape character '\%' [-Wpedantic]
-	zoom_value_->setMinimumWidth(fm.horizontalAdvance("444\%"));
+	zoom_value_->setMinimumWidth(fm.horizontalAdvance("444M"));
 #else
-	zoom_value_->setMinimumWidth(fm.width("444\%"));
+	zoom_value_->setMinimumWidth(fm.width("444M"));
 #endif
 	zoom_value_->setAlignment(Qt::AlignCenter);
 	zoom_value_->setText(toqstr(bformat(_("[[ZOOM]]%1$d%"), zoom)));
