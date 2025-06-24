aba40a9ecf01fb0815ec371df4eefa2cf2eef8d3
Juergen Spitzmueller <spitz@lyx.org>
2025-06-24 13:41:27 +0200

    Amend 6acc0759fc89
    for the case of read-only documents (though I think the real
    problem here being the restore state wrong in the first place).
    
    This should obviously go to stable, too.

--- src/frontends/qt/GuiDocument.cpp
+++ src/frontends/qt/GuiDocument.cpp
@@ -1893,7 +1893,7 @@ GuiDocument::GuiDocument(GuiView & lv)
 
 void GuiDocument::checkOnClosing()
 {
-	if (!bc().policy().buttonStatus(ButtonPolicy::RESTORE))
+	if (buffer().isReadonly() || !bc().policy().buttonStatus(ButtonPolicy::RESTORE))
 		// nothing to do
 		return;
 
