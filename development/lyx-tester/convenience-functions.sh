8a5c53c2b2075e7c5572485aba2c648462be6ac6
Scott Kostyshak <skostysh@lyx.org>
2025-07-31 11:06:08 +0200

    lyx-tester: fn to register GCC and Clang versions
    The new function 'update-alternatives-add-new-gcc-and-clang' makes
    it easy to register all available GCC and Clang versions.
    
    This makes it possible to use 'switch-gcc-to' and 'switch-clang-to',
    which are defined in the 'switches' file.

--- development/lyx-tester/convenience-functions.sh
+++ development/lyx-tester/convenience-functions.sh
@@ -10,3 +10,43 @@ function git-grep-accel-robust ()
   search_str="$( echo "$1" | sed 's/.\{1\}/&\&\\\?/g')"
   git grep -i "${search_str}"
 }
+
+
+function update-alternatives-add-new-gcc-and-clang ()
+{
+
+  # It is in theory fine to run this multiple times. i.e., shouldn't affect
+  # anything if it's already installed.
+
+  # todo: should we clear previous ones?
+  #
+  # These will give errors (and exit) the first time, i.e., when there are no
+  # alternatives set:
+  #
+  #   update-alternatives: error: no alternatives for clang++
+  #
+  #sudo update-alternatives --remove-all gcc
+  #sudo update-alternatives --remove-all g++
+  #sudo update-alternatives --remove-all gfortran
+  #
+  #sudo update-alternatives --remove-all clang
+  #sudo update-alternatives --remove-all clang++
+
+  # Order is important. The last one will be the default.
+  gcc_versions=( $(ls /usr/bin/gcc-* | grep -o -P '(?<=gcc-)\d+(?=$)') )
+  for v in "${gcc_versions[@]}"; do
+    echo "${v}"
+    sudo update-alternatives --install "/usr/bin/gcc" "gcc" "/usr/bin/gcc-${v}" "${v}"
+    sudo update-alternatives --install "/usr/bin/g++" "g++" "/usr/bin/g++-${v}" "${v}"
+    sudo update-alternatives --install "/usr/bin/gfortran" "gfortran" "/usr/bin/gfortran-${v}" "${v}"
+  done
+
+  clang_versions=( $(ls /usr/bin/clang-* | grep -o -P '(?<=clang-)\d+(?=$)') )
+  for v in "${clang_versions[@]}"; do
+    echo "${v}"
+    sudo update-alternatives --install "/usr/bin/clang" "clang" "/usr/bin/clang-${v}" "${v}"
+    sudo update-alternatives --install "/usr/bin/clang++" "clang++" "/usr/bin/clang++-${v}" "${v}"
+  done
+
+  echo -e "now you can use 'switch-gcc-to' and 'switch-clang-to' (from lyx-tester switches)"
+}
